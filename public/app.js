cd381938e14c53d194022c230a5d55ce946b574d
Mark Reed <markdreed512@gmail.com>
2019-04-23 13:19:53 -0500

    capture user note input

--- public/app.js
+++ public/app.js
@@ -1,8 +1,54 @@
-$.getJSON("/articles", function(data) {
-    console.log(data)
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<h2>" + data[i].headline+ "</h2>" + "<p>" + data[i].summary + "</p>" + "<p><a href=" + data[i].url + ">read more...</a></p>");
-    }
-  });
+// $.getJSON("/articles", function(data) {
+//     console.log(data)
+//     // For each one
+//     for (var i = 0; i < data.length; i++) {
+//       // Display the apropos information on the page
+//       $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<h2>" + data[i].headline+ "</h2>" + "<p>" + data[i].summary + "</p>" + "<p><a href=" + data[i].url + ">read more...</a></p>");
+//     }
+//   });
+
+//put 
+
+//When you click comment button, grab data-id and assign to  variable. capture input and insert into db
+
+$(".comment-btn").on('click', function (event) {
+   //capture value in input field
+   var dataId = event.target.getAttribute("data-id");
+   var userInput = {
+       note: $("#" + dataId).val().trim()
+   };
+   console.log("user input: ", userInput);
+   
+   //send to controller
+//    $.ajax("/api/burger" , {
+//        type: "POST",
+//        data: userInput
+//    }).then(
+//        function () {
+//            console.log("created new burger");
+//            // Reload the page to get the updated list
+//            location.reload();
+//        }
+//    );
+});
+
+// $("#submitBtn").on('click', function (event) {
+//     event.preventDefault();
+//    //capture value in input field
+//    var userInput = {
+//        name: $('#userInput').val().trim()
+//    }
+   
+//    console.log("user input: ", userInput);
+//    //send to controller
+//    $.ajax("/api/burger" , {
+//        type: "POST",
+//        data: userInput
+//    }).then(
+//        function () {
+//            console.log("created new burger");
+//            // Reload the page to get the updated list
+//            location.reload();
+//        }
+//    );
+// });
--- server.js
+++ server.js
@@ -25,7 +25,14 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// A GET route for scraping the hardtimes website
+// A GET route for scraping the hardtimes website and saving to db
+// Go back to Saturday's activities if you need a refresher on how to partner one model with another.
+
+// Whenever you scrape a site for stories, make sure an article isn't already represented in your database before saving it; Do not save any duplicate entries.
+
+// Don't just clear out your database and populate it with scraped articles whenever a user accesses your site.
+
+// If your app deletes stories every time someone visits, your users won't be able to see any comments except the ones that they post.
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
     axios.get("http://thehardtimes.net").then(function (response) {
@@ -72,7 +79,6 @@ app.get("/articles", function(req, res) {
         var hbsObject = {
             articles: dbArticles
           };
-          console.log("hb object-dbArticles: ", dbArticles)
           res.render("index", hbsObject);
       })
       .catch(function(err) {
@@ -80,6 +86,27 @@ app.get("/articles", function(req, res) {
         res.json(err);
       });
   });
+
+  
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
